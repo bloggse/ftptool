aadce84675dfa98f4e2d27c8121aab8c3ee520aa
Daniel Brandt <me@dbrandt.se>
2014-04-17 10:05:22 +0200

    setup.py updates

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ readme_fname = os.path.join(os.path.dirname(__file__), "README.rst")
 readme_text = open(readme_fname).read()
 
 setup(name="ftptool", version="0.6",
-    url="http://blogg.se",
+    url="https://github.com/bloggse/ftptool",
     description="Higher-level interface to ftplib",
     author="Blogg Esse AB",
-    author_email="opensource@blogg.se",
+    author_email="teknik@blogg.se",
     long_description=readme_text,
     py_modules=["ftptool"])
